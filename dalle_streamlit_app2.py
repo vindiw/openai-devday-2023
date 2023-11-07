f8f2a11131be2c243ff45c9cafbde9c6db39b50b
Vindiw Wijesooriya <tivainthespeedcuber@gmail.com>
2023-11-07 19:30:38 +0530

    started saving images because image_url expires in 2mins
    :')

--- dalle_streamlit_app2.py
+++ dalle_streamlit_app2.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import openai
 import os
-import requests
 from PIL import Image
-from io import BytesIO
 import sqlite3
 from datetime import datetime
 import pytz
+from io import BytesIO
+import requests
 
 # Define your local timezone
 local_tz = pytz.timezone('Asia/Colombo')  # For Sri Lanka
@@ -16,43 +16,44 @@ def utc_to_local(utc_dt):
     local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
     return local_tz.normalize(local_dt)
 
-# Initialize the database and table if they don't exist
 def init_db():
     with sqlite3.connect('dalle_images.db') as conn:
-        # Create table if it does not exist
         conn.execute('''
             CREATE TABLE IF NOT EXISTS images (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 prompt TEXT NOT NULL,
-                image_url TEXT NOT NULL,
-                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+                image_binary BLOB,
+                image_url TEXT,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                revised_prompt TEXT
             );
         ''')
-        # Add revised_prompt column if it does not exist
         try:
-            conn.execute('ALTER TABLE images ADD COLUMN revised_prompt TEXT')
+            conn.execute('ALTER TABLE images ADD COLUMN image_url TEXT')
         except sqlite3.OperationalError:
-            # If the error is because the column already exists, it will pass
-            pass
+            pass  # Column already exists, ignore
 
-# Function to insert a new record into the database
-def insert_image(prompt, revised_prompt, image_url):
+# Modify your insert_image function to accept and store binary image data
+def insert_image(prompt, revised_prompt, image_binary, image_url):
     with sqlite3.connect('dalle_images.db') as conn:
-        conn.execute('INSERT INTO images (prompt, revised_prompt, image_url) VALUES (?, ?, ?)', 
-                     (prompt, revised_prompt, image_url))
+        conn.execute('INSERT INTO images (prompt, revised_prompt, image_binary, image_url) VALUES (?, ?, ?, ?)', 
+                     (prompt, revised_prompt, image_binary, image_url))
 
 # Function to get all image records
 def get_all_images():
     with sqlite3.connect('dalle_images.db') as conn:
         cur = conn.cursor()
-        cur.execute('SELECT prompt, revised_prompt, image_url, timestamp FROM images ORDER BY timestamp DESC')
+        cur.execute('SELECT id, prompt, revised_prompt, image_binary, image_url, timestamp FROM images ORDER BY timestamp DESC')
         return cur.fetchall()
 
-# Set your OpenAI API key (preferably use an environment variable for this)
+# Set your OpenAI API key
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Initialize database
 init_db()
+
+# Initialize OpenAI Client
+client = openai.OpenAI(api_key=openai.api_key)
 
 st.title('DALL-E 3 Image Generator')
 
@@ -63,30 +64,38 @@ page = st.sidebar.selectbox("Choose a page", ["Generate", "Gallery"])
 # Page for image generation
 if page == "Generate":
     prompt = st.text_input('Enter the prompt for the image you want to generate:', '')
+    size = st.selectbox('Select image size:', ['1024x1024', '1024x1792', '1792x1024'])
+    quality = st.selectbox('Select image quality:', ['standard', 'hd'])
 
     if st.button('Generate Image'):
         if prompt:
             try:
-                response = openai.Image.create(
+                response = client.images.generate(
                     model="dall-e-3",
                     prompt=prompt,
-                    size="1024x1024",
-                    quality="standard",
-                    n=1,
+                    size=size,
+                    quality=quality,
+                    n=1
                 )
-                image_data = response['data'][0]
-                image_url = image_data['url']
-                revised_prompt = image_data.get('revised_prompt', 'No revised prompt provided')
+                # Assuming the response object has a 'data' attribute which is a list of Image objects.
+                image_data = response.data[0].url  # This should be the correct way to access the URL
+                revised_prompt = getattr(response.data[0], 'revised_prompt', 'No revised prompt provided')
 
-                insert_image(prompt, revised_prompt, image_url)
-
-                image_response = requests.get(image_url)
+                # Convert image URL to binary data and save as PNG
+                image_response = requests.get(image_data)
                 image = Image.open(BytesIO(image_response.content))
+                buffered = BytesIO()
+                image.save(buffered, format="PNG")
+                image_binary = buffered.getvalue()
+
+                insert_image(prompt, revised_prompt, image_binary, image_data)  # Pass the image URL here
+
                 st.image(image, caption='Generated Image', use_column_width=True)
-                st.caption(f"Revised prompt: {revised_prompt}")  # Show the revised prompt under the image
+                st.caption(f"Revised prompt: {revised_prompt}")
 
             except Exception as e:
                 st.error(f"An error occurred: {e}")
+
         else:
             st.warning('Please enter a prompt.')
 
@@ -95,10 +104,18 @@ elif page == "Gallery":
     st.title("Image Gallery")
 
     all_images = get_all_images()
-    for idx, (prompt, revised_prompt, image_url, timestamp_str) in enumerate(all_images):
-        utc_timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
-        local_timestamp = utc_to_local(utc_timestamp)
+    for idx, (id, prompt, revised_prompt, image_binary, image_url, timestamp_str) in enumerate(all_images):
+        if image_binary:
+            try:
+                image = Image.open(BytesIO(image_binary))
+                utc_timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+                local_timestamp = utc_to_local(utc_timestamp)
 
-        st.subheader(f"Prompt: {prompt}")
-        st.caption(f"Revised Prompt: {revised_prompt}")  # Display the revised prompt in fine print
-        st.image(image_url, caption=f"Generated on {local_timestamp.strftime('%Y-%m-%d %H:%M:%S')}", use_column_width=True)
+                st.subheader(f"Prompt: {prompt}")
+                st.caption(f"Revised Prompt: {revised_prompt}")
+                st.image(image, caption=f"Generated on {local_timestamp.strftime('%Y-%m-%d %H:%M:%S')}", use_column_width=True)
+            except Exception as e:
+                st.error(f"Could not load image for prompt: '{prompt}'. Error: {e}")
+        else:
+            st.info(f"No image data available for prompt: '{prompt}'.")
+
